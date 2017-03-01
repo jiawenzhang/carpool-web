f786ca1d1e50dd0e2341bd3f4d770120c1134b4e
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2017-03-01 00:20:20 -0500

    Add wechat validate function

--- devServer.js
+++ devServer.js
@@ -3,6 +3,8 @@ var express = require('express');
 var router = express.Router();
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
+var crypto = require("crypto");
+var url = require("url");
 
 var app = express();
 var compiler = webpack(config);
@@ -25,6 +27,40 @@ router.get('/load/', function(req, res) {
     var link = decodeURIComponent(params.link);
     console.log("link: " + link);
 });
+
+router.get('/validate/', function(req, res) {
+  console.log("validate")
+  var query = url.parse(req.url, true).query;
+  //console.log("*** URL:" + req.url);
+  //console.log(query);
+  var signature = query.signature;
+  var echostr = query.echostr;
+  var timestamp = query['timestamp'];
+  var nonce = query.nonce;
+  var oriArray = new Array();
+  oriArray[0] = nonce;
+  oriArray[1] = timestamp;
+  oriArray[2] = "wechat" //这里是你在微信开发者中心页面里填的token，而不是****
+  oriArray.sort();
+  var original = oriArray.join('');
+  console.log("Original str : " + original);
+  console.log("Signature : " + signature );
+  var scyptoString = sha1(original);
+  if(signature == scyptoString){
+    res.end(echostr);
+    console.log("Confirm and send echo back");
+  }else {
+    res.end("false");
+    console.log("Failed!");
+  }
+});
+
+function sha1(str){
+  var md5sum = crypto.createHash("sha1");
+  md5sum.update(str);
+  str = md5sum.digest("hex");
+  return str;
+}
 
 var httpServer = require('http').createServer(app);
 httpServer.listen(process.env.PORT || 3000, function() {
